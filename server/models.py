a362e8bf5f50eae8421232289f9c0b7f51546eac
Anthony <anthony.onyango@student.moringaschool.com>
2024-10-10 00:09:41 +0300

    completed the lab

--- server/models.py
+++ server/models.py
@@ -1,45 +1,42 @@
-# server/models.py
+import datetime
+from app import app
+from models import db, Employee, Review, Onboarding
 
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+with app.app_context():
 
-metadata = MetaData(
-    naming_convention={
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    }
-)
+    # Delete all rows in tables
+    Employee.query.delete()
+    Review.query.delete()
+    Onboarding.query.delete()
 
-db = SQLAlchemy(metadata=metadata)
+    # Add model instances to database
+    uri = Employee(name="Uri Lee", hire_date=datetime.datetime(2022, 5, 17))
+    tristan = Employee(name="Tristan Tal", hire_date=datetime.datetime(2020, 1, 30))
+    db.session.add_all([uri, tristan])
+    db.session.commit()
 
+    # 1..many relationship between Employee and Review
+    uri_2023 = Review(year=2023, summary="Great web developer!", employee=uri)
+    tristan_2021 = Review(
+        year=2021, summary="Good coding skills, often late to work", employee=tristan
+    )
+    tristan_2022 = Review(
+        year=2022, summary="Strong coding skills, takes long lunches", employee=tristan
+    )
+    tristan_2023 = Review(
+        year=2023, summary="Awesome coding skills, dedicated worker", employee=tristan
+    )
+    db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
+    db.session.commit()
 
-class Employee(db.Model):
-    __tablename__ = "employees"
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    hire_date = db.Column(db.Date)
-
-    def __repr__(self):
-        return f"<Employee {self.id}, {self.name}, {self.hire_date}>"
-
-
-class Onboarding(db.Model):
-    __tablename__ = "onboardings"
-
-    id = db.Column(db.Integer, primary_key=True)
-    orientation = db.Column(db.DateTime)
-    forms_complete = db.Column(db.Boolean, default=False)
-
-    def __repr__(self):
-        return f"<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>"
-
-
-class Review(db.Model):
-    __tablename__ = "reviews"
-
-    id = db.Column(db.Integer, primary_key=True)
-    year = db.Column(db.Integer)
-    summary = db.Column(db.String)
-
-    def __repr__(self):
-        return f"<Review {self.id}, {self.year}, {self.summary}>"
+    # 1..1 relationship between Employee and Onboarding
+    uri_onboarding = Onboarding(
+        orientation=datetime.datetime(2023, 3, 27), employee=uri
+    )
+    tristan_onboarding = Onboarding(
+        orientation=datetime.datetime(2020, 1, 20, 14, 30),
+        forms_complete=True,
+        employee=tristan,
+    )
+    db.session.add_all([uri_onboarding, tristan_onboarding])
+    db.session.commit()
